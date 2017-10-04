613bdc04f63830c8eea5aa0b89a963302072ccb0
valdiney <valdiney.2@hotmail.com>
2017-10-04 08:47:51 -0300

    Create a function to create de data base

--- createDatabase.py
+++ createDatabase.py
@@ -1,6 +1,10 @@
 import sqlite3
 
 # Cria um banco de dados no diretório local
-conn = sqlite3.connect('teste.db')
+def createDatabase():
+    conn = sqlite3.connect('teste.db')
+    conn.close()
 
-conn.close()
+
+
+createDatabase()
