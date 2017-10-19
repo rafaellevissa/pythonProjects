1b6f9e6c719d183bf91ba8bf735024e84de8799f
Valdiney <valdiney.2@hotmail.com>
2017-10-19 21:48:32 -0200

    Script para criar a tabela 1

--- createTable.py
+++ createTable.py
@@ -27,6 +27,25 @@ def createTableLinhaProducao(cursor):
 		);
 	""")
 
+# Cria a tabela1
+def createTable1(cursor):
+	cursor.execute("""
+	CREATE TABLE tab1 (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		linha INT NOT NULL,
+		contador INT NOT NULL,
+		meta INT NOT NULL,
+		tempo_ciclo INT NOT NULL,
+		hora_inicio INT NOT NULL,
+		minuto_inicio INT NOT NULL,
+		hora_termino INT NOT NULL,
+		minuto_termino INT NOT NULL,
+		tempo_refresh_realizado INT NOT NULL,
+		tempo_refresh_homem INT NOT NULL,
+		created_at TIMESTAMP
+		);
+	""")
+
 try:
     createTableLinhaProducao(cursor)
     print("Tabela Linha de producao criada com Sucesso!")
@@ -39,3 +58,10 @@ try:
     print("Tabela Producao criada com Sucesso!")
 except Exception as e:
 	print("Erro ao tentar criar a Tabela Producao: ", e)
+
+
+try:
+    createTable1(cursor)
+    print("Tabela tab1 criada com Sucesso!")
+except Exception as e:
+    print("Erro ao tentar criar a Tabela tab1: ", e)
